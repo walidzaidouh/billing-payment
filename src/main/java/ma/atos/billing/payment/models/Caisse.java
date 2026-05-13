2014a205ae30920c938999b34a342be9e0cc238c
omar hafidi alaoui <ohafidi.oh@gmail.com>
2026-05-13 12:14:41 +0100

    modifs

--- src/main/java/ma/atos/billing/payment/models/Caisse.java
+++ src/main/java/ma/atos/billing/payment/models/Caisse.java
@@ -1,10 +1,18 @@
 package ma.atos.billing.payment.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import java.sql.Date;
 
+@Id
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Caisse {
 
     private Long id  ;
--- src/main/java/ma/atos/billing/payment/models/PointDeVente.java
+++ src/main/java/ma/atos/billing/payment/models/PointDeVente.java
@@ -1,4 +1,16 @@
 package ma.atos.billing.payment.models;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Id
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class PointDeVente {
 }
