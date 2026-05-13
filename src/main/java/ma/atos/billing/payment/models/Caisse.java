8cbfb27e7a618cd52a58df9c7ae431893b6e0038
omar hafidi alaoui <ohafidi.oh@gmail.com>
2026-05-13 12:01:15 +0100

    added classes

---
+++ src/main/java/ma/atos/billing/payment/models/Caisse.java
@@ -0,0 +1,12 @@
+package ma.atos.billing.payment.models;
+
+import jakarta.persistence.Entity;
+
+import java.sql.Date;
+
+@Entity
+public class Caisse {
+
+    private Long id  ;
+    private Date date ;
+}
