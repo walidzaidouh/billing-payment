9d9c08d249d245fde8c194d81a569cfca3d0812c
Zakaria HAJJI <zakaria.hajji@atos.net>
2026-05-14 14:24:22 +0100

    Add function getAllTransByCustomer controller

--- src/main/java/ma/atos/billing/payment/Controllers/TransactionController.java
+++ src/main/java/ma/atos/billing/payment/Controllers/TransactionController.java
@@ -1,10 +1,26 @@
 package ma.atos.billing.payment.Controllers;
 
 
+import ma.atos.billing.payment.Services.TransactionService;
+import ma.atos.billing.payment.models.Transaction;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api/transactions")
 public class TransactionController {
+private final TransactionService transactionService;
 
-
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+    @GetMapping("/customer/{id}")
+    public List<Transaction> getTransactionsByCustomer(@PathVariable Long id){
+        return transactionService.getAllTransByCustomer(id);
+    }
 }
+
