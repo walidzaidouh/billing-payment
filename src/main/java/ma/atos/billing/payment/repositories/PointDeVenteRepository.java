8f73b4d1b898999befae5af7411271287b540f3e
omar hafidi alaoui <ohafidi.oh@gmail.com>
2026-05-14 16:50:52 +0100

    update

--- src/main/java/ma/atos/billing/payment/repositories/PointDeVenteRepository.java
+++
@@ -1,9 +0,0 @@
-package ma.atos.billing.payment.repositories;
-
-import ma.atos.billing.payment.models.PointDeVente;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PointDeVenteRepository extends JpaRepository<PointDeVente, Long> {
-}
