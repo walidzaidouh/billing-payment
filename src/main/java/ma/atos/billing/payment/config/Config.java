8b7a937509cf4241455399c96f6cf00543e4ccf4
wzaidouh <walidzaidouh@gmail.com>
2026-05-14 17:08:31 +0100

    feat-dev-add-vault

--- src/main/java/ma/atos/billing/payment/config/Config.java
+++ src/main/java/ma/atos/billing/payment/config/Config.java
@@ -1,4 +1,20 @@
 package ma.atos.billing.payment.config;
 
+
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Log4j2
+@Configuration
 public class Config {
+
+
+    @Value("${spring.datasource.username:}")
+    private String usernameVault;
+
+    public void test(){
+    System.out.println("Test Vault " + usernameVault);
+    }
 }
